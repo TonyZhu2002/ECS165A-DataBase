32d3e8299a70e6799dc6687900b72cdf97f16452
CATGPT0 <133710245+CATGPT0@users.noreply.github.com>
2024-02-07 08:57:09 -0800

    Update page.py
    Import config to avoid hard code.

--- lstore/page.py
+++ lstore/page.py
@@ -1,13 +1,14 @@
+from lstore.config import *
 import struct
 
 class Page:
 
     def __init__(self):
         self.num_records = 0
-        self.data = bytearray(4096)
+        self.data = bytearray(MAX_PAGE_SIZE)
 
     def has_capacity(self):
-        return self.num_records < 1024
+        return self.num_records < MAX_RECORD_PER_PAGE
 
     def write(self, value):
         if self.has_capacity:
