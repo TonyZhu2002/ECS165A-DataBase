f5e192268f307efe08f78fd258b0177c8787fc8e
TonyZhu2002 <zhu.ming.yu.1015@gmail.com>
2024-03-15 21:48:39 -0700

    Hi

--- lstore/transaction.py
+++ lstore/transaction.py
@@ -12,6 +12,7 @@ class Transaction:
         """
         self.queries = []
         self.old_value_keeper = []
+        self.table = None
         pass
 
     def add_query(self, query, table, *args):
@@ -23,6 +24,7 @@ class Transaction:
         # t.add_query(q.update, grades_table, 0, *[None, 1, None, 2, None])
         """
         self.queries.append((query, args))
+        self.table = table
         # use grades_table for aborting
 
     # If you choose to implement this differently this method must still return True if transaction commits or False on abort
@@ -34,12 +36,22 @@ class Transaction:
                 return self.abort()
             self.old_value_keeper.append(self.rollback_method(query, args))
         return self.commit()
+
     def rollback_method(self, function, *args):
+        query_name = function.__name__
+        if query_name == "insert":
+            return self.queries.delete, args
+            pass
+        elif query_name == "delete":
+            pass
+        elif query_name == "update":
+            pass
         pass
+
         # Assume it returns a tuple similar to add_query, but contains rollback function with necessary arguments.
     def abort(self):
         # TODO: do roll-back and any other necessary operations
-        for query, args in self.old_value_keeper:
+        for query, args in reversed(self.old_value_keeper):
             query(*args)
         self.old_value_keeper.clear()
         return False
